3e5d9f18efbff91e14d0085c8ed630aeedfae3a0
Diana Castillo <dianahervascastillo@gmail.com>
2019-04-28 12:47:49 +0100

    fiex ie11 stuff i forgot

--- src/components/AddNewCard/AddNewCard.js
+++ src/components/AddNewCard/AddNewCard.js
@@ -5,19 +5,17 @@ class AddNewCard extends Component {
   render() {
     return (
       <div className='card add-new-idea'>
-        <form>
-          <header className='card-header'>
-            <input className='input' placeholder="Title for your idea" maxLength='28' ref={this.props.newTitleRef} type='text' value={this.props.newTitle} onChange={this.props.onAddTitle} />
-          </header>
-          <div className='card-content'>
-            <div className='content'>
-              <textarea className='textarea' placeholder='Describe your idea' maxLength='140' rows='5' ref={this.props.newDescriptionRef} value={this.props.newDescription} onChange={this.props.onAddDescription}/>
-            </div>
+        <header className='card-header'>
+          <input className='input' placeholder="Title for your idea" maxLength='28' ref={this.props.newTitleRef} type='text' value={this.props.newTitle} onChange={this.props.onAddTitle} />
+        </header>
+        <div className='card-content'>
+          <div className='content'>
+            <textarea className='textarea' placeholder='Describe your idea' maxLength='140' rows='5' ref={this.props.newDescriptionRef} value={this.props.newDescription} onChange={this.props.onAddDescription}/>
           </div>
-          <footer className='card-footer'>
-            <button className='button is-primary' type="submit" onClick={this.props.onSubmit}>Add Idea</button>
-          </footer>
-        </form>
+        </div>
+        <footer className='card-footer'>
+          <button className='button is-primary' type="submit" onClick={this.props.onSubmit}>Add Idea</button>
+        </footer>
       </div>
     );
   }
